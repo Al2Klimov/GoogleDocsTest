0e768568e9da2e95e685ad41006d561b80361f51
Alexander A. Klimov <grandmaster@al2klimov.de>
2021-08-28 22:41:19 +0200

    Skeleton

---
+++ app/src/main/java/de/al2klimov/android/googledocstest/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.al2klimov.android.googledocstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
